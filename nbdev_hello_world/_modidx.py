cd753472da5c330f7a3cf63b18a0c343d5bb1136
Huy Tran <htran8899@gmail.com>
2023-10-01 14:11:06 -0500

    Add say_hello; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://hbtran.github.io',
                 'git_url': 'https://github.com/hbtran/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.hello-world-core': { 'nbdev_hello_world.hello-world-core.foo': ( 'hello_world_core.html#foo',
+                                                                                                'nbdev_hello_world/hello-world-core.py'),
+                                                    'nbdev_hello_world.hello-world-core.say_hello': ( 'hello_world_core.html#say_hello',
+                                                                                                      'nbdev_hello_world/hello-world-core.py')}}}
